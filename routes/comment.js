917fa15f1527aa3df7ec704b0e05489209104a9e
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-09-20 11:13:13 -0400

    Added Comment section and delete feature

--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Comment Routes
 router.post("/createComment/:id", commentsController.createComment);
-
+router.delete("/deleteComment/:postid/:commentid", commentsController.deleteComment);
+// parameters in path come from the req.params in our controller
 
 module.exports = router;
